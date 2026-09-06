ab5674b5e856ac50972f7ca6ff1cbd1764ae36be
mirkocuchan <mirko.cuchan01@gmail.com>
2026-09-06 15:43:17 +0000

    removing unused

--- main.go
+++ main.go
@@ -20,10 +20,7 @@ import (
 type apiConfig struct {
 	DB *database.Queries
 }
-func unused() {
-    // this function does nothing
-    // and is called nowhere
-}
+
 //go:embed static/*
 var staticFiles embed.FS
 
